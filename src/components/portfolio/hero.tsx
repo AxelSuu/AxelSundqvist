51f13e4ff1fe9e4b684467752cb47c1601178102
Axel Sundqvist <axesu672@student.liu.se>
2025-12-27 21:35:33 +0100

    Fixed hero section
    - Removed a textline

--- src/components/portfolio/hero.tsx
+++ src/components/portfolio/hero.tsx
@@ -108,13 +108,7 @@ const Hero = () => {
                 delay={800}
               />
               <br />
-              <span className="inline-flex items-center gap-2 mt-2 text-green-500 font-medium">
-                <span className="relative flex h-3 w-3">
-                  <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"></span>
-                  <span className="relative inline-flex rounded-full h-3 w-3 bg-green-500"></span>
-                </span>
-                Available for Summer 2026 Internships
-              </span>
+              
             </div>
             
             {/* Social links with magnetic effect */}
